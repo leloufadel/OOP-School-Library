f13a9bdd2971218d4e71d55ad154fe12b64a584b
leloufadel <leloufadel@gmail.com>
2023-09-04 20:24:48 +0300

    add person class

---
+++ person.rb
@@ -0,0 +1,23 @@
+attr_accessor :name, :age
+attr_reader :id
+class Person
+    def initialize(id, age, name = "Unknown", parent_permission ="true")
+      @id = Random.rand(1..1000)
+      @name = name
+      @parent_permission = parent_permission
+      @age = age
+    end
+  end
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || parent_permission
+  end
+end
+    
